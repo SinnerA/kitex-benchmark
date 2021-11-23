baa689048610fdca75682788579c9c753d8d73d0
jiangxuewu <jiangxuewu@bytedance.com>
2021-11-23 17:28:42 +0800

    chore: using develop branch when building pb
    Change-Id: I737fb9a8772393f25df0ef4b3f830557d4a50676

--- scripts/build_pb.sh
+++ scripts/build_pb.sh
@@ -7,6 +7,8 @@ rm -rf output/ && mkdir -p output/bin/ && mkdir -p output/log/
 
 # build kitex
 $GOEXEC mod edit -replace=github.com/apache/thrift=github.com/apache/thrift@v0.13.0
+$GOEXEC mod edit -replace=github.com/cloudwego/kitex=github.com/cloudwego/kitex@3af6c384e202323e43f6f89fb08146c6013fb1f0
+$GOEXEC mod edit -replace=github.com/cloudwego/netpoll-http2=github.com/cloudwego/netpoll-http2@5756b8c33a14f561ea213d4a2b5cbaa52ef365cd
 $GOEXEC mod tidy
 $GOEXEC build -v -o output/bin/kitex_bencher ./protobuf/kitex/client
 $GOEXEC build -v -o output/bin/kitex-mux_bencher ./protobuf/kitex-mux/client
